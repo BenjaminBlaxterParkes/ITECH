3e9075ea266e7bb7fc48c22e933ca62c4ec718e3
Ben Parkes <benjamin.b.parkes@googlemail.com>
2018-01-29 20:04:05 +0000

    chp6 complete, page urls still not perfect

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -11,34 +11,42 @@ def populate():
 
 	python_pages = [
 		{"title": "Official Python Tutorial",
-		 "url":"http://docs.python.org/2/tutorial/"},
+		 "url":"http://docs.python.org/2/tutorial/",
+         "views": 32},
     	{"title": "How to Think like a Computer Scientist",
-    	 "url":"http://www.greenteapress.com/thinkpython/"},
+    	 "url":"http://www.greenteapress.com/thinkpython/",
+         "views": 16},
     	{"title": "Learn Python in 10 Minutes",
-    	 "url":"http://www.korokithakis.net/tutorials/python/"} ]
+    	 "url":"http://www.korokithakis.net/tutorials/python/",
+         "views": 8} ]
 
 	django_pages = [
     	{"title": "Official Django Tutorial",
-    	 "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+    	 "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 64},
     	{"title": "Django Rocks",
-    	 "url":"http://www.djangorocks.com/"},
+    	 "url":"http://www.djangorocks.com/",
+         "views": 16},
     	{"title": "How to Tango with Django",
-    	 "url":"http://www.tangowithdjango.com/"} ]
+    	 "url":"http://www.tangowithdjango.com/",
+         "views": 32} ]
 
 	other_pages = [
     	{"title": "Bottle",
-    	 "url":"http://bottlepy.org/docs/dev/"},
+    	 "url":"http://bottlepy.org/docs/dev/",
+         "views": 32},
     	{"title": "Flask",
-    	 "url":"http://flash.pocoo.org"} ]
+    	 "url":"http://flash.pocoo.org",
+         "views": 16} ]
 
-	cats = {"Python": "128": "64": {"pages": python_pages},
-    		"Django": "64": "32": {"pages": django_pages},
-    		"Other Frameworks": "32": "16": {"pages": other_pages} }
+	cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+    		"Django": {"pages": django_pages, "views": 64, "likes": 32},
+    		"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
 
 	for cat, cat_data in cats.items():
-   		c = add_cat(cat)
+   		c = add_cat(cat, cat_data["views"], cat_data["likes"])
    		for p in cat_data["pages"]:
-   			add_page(c, p["title"], p["url"])
+   			add_page(c, p["title"], p["url"], p['views'])
 
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
@@ -51,12 +59,12 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name,views,likes):
-	c = Category.objects.get_or_create(name=name)[0]
-	c.views.get_or_create(views=views)[0]
-	c.likes.get_or_create(likes=likes)[0]
-	c.save()
-	return c
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
+    c.save()
+    return c
 
 if __name__ == '__main__':
 	print("Starting Rango population script...")
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
-        url(r'^about/$', views.about, name='about'),
-        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
