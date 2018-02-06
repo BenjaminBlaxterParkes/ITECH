f65b7dbd8ecdc2c2b402edefd78c2fcc4fdd0ddc
Ben Parkes <benjamin.b.parkes@googlemail.com>
2018-02-06 16:17:13 +0000

    Chapter 8 complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	# context_dict = {'boldmessage': "Crunchy, cream, cookie, candy, cupcake!"}
@@ -13,8 +14,8 @@ def index(request):
 )
 
 def about(request):	
-	return render(request, 'rango/about.html')
-	return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	return render(request, 'rango/about.html', {})
+	# return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 
 def show_category(request, category_name_slug):
 	context_dict = {}
@@ -27,4 +28,37 @@ def show_category(request, category_name_slug):
 		context_dict['pages'] = None
 		context_dict['category'] = None
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	form = CategoryForm
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+			return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+	return render(request, 'rango/add_page.html', {'form': form, 'category': category})
 
