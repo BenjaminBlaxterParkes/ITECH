03778eb7945fa7a096a925447335f91fb6ce3ece
Ben Parkes <benjamin.b.parkes@googlemail.com>
2018-02-09 16:29:37 +0000

    chp 10 complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,18 +6,52 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, defult_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = defult_val
+	return val
+
+
+def vistitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		visits = 1
+		# response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
+	# response.set_cookie('visits', visits)
+	request.session['visits'] = visits
+
 
 def index(request):
 	# context_dict = {'boldmessage': "Crunchy, cream, cookie, candy, cupcake!"}
+	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
-	return render(request, 'rango/index.html', context=context_dict)
-	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>"
-)
+
+	vistitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
+	response = render(request, 'rango/index.html', context=context_dict)
+# 	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>"
+# )
+	return response
 
 
 def about(request):	
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
 	return render(request, 'rango/about.html', {})
 	# return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 
@@ -132,3 +166,8 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
+
+
+
+
+
